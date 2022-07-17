c92ee7fe2b20c661aad4c39137d8560e909473a1
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2022-07-18 00:52:25 +0530

    Initial commit

--- src/components/NewsCards/NewsCard/NewsCard.js
+++ src/components/NewsCards/NewsCard/NewsCard.js
@@ -1,10 +1,21 @@
-import React, { useState, useEffect, createRef } from 'react';
-import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import React, { useState, useEffect, createRef } from "react";
+import {
+  Card,
+  CardActions,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@mui/material";
 
+import useStyles from "./styles";
 
-import useStyles from './styles';
-
-const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, activeArticle, i }) => {
+const NewsCard = ({
+  article: { description, publishedAt, source, title, url, urlToImage },
+  activeArticle,
+  i,
+}) => {
   const classes = useStyles();
   const [elRefs, setElRefs] = useState([]);
   const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50);
@@ -12,7 +23,11 @@ const NewsCard = ({ article: { description, publishedAt, source, title, url, url
   useEffect(() => {
     window.scroll(0, 0);
 
-    setElRefs((refs) => Array(20).fill().map((_, j) => refs[j] || createRef()));
+    setElRefs((refs) =>
+      Array(20)
+        .fill()
+        .map((_, j) => refs[j] || createRef())
+    );
   }, []);
 
   useEffect(() => {
@@ -22,21 +37,48 @@ const NewsCard = ({ article: { description, publishedAt, source, title, url, url
   }, [i, activeArticle, elRefs]);
 
   return (
-    <Card ref={elRefs[i]} className={ activeArticle === i ? classes.activeCard : classes.card}>
+    <Card
+      ref={elRefs[i]}
+      className={activeArticle === i ? classes.activeCard : classes.card}
+    >
       <CardActionArea href={url} target="_blank">
-        <CardMedia className={classes.media} image={urlToImage || 'https://www.industry.gov.au/sites/default/files/August%202018/image/news-placeholder-738.png'} title={title} />
+        <CardMedia
+          className={classes.media}
+          image={
+            urlToImage ||
+            "https://www.industry.gov.au/sites/default/files/August%202018/image/news-placeholder-738.png"
+          }
+          title={title}
+        />
         <div className={classes.details}>
-          <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
-          <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {new Date(publishedAt).toDateString()}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {source.name}
+          </Typography>
         </div>
-        <Typography className={classes.title} gutterBottom variant="h5" component="h2">{title}</Typography>
+        <Typography
+          className={classes.title}
+          gutterBottom
+          variant="h5"
+          component="h2"
+        >
+          {title}
+        </Typography>
         <CardContent>
-          <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions className={classes.cardActions}>
-        <Button size="small" color="primary" href={url}>Learn More</Button>
-        <Typography variant="h5" color="textSecondary" component="h2">{i + 1}</Typography>
+        <Button size="small" color="primary" href={url}>
+          Learn More
+        </Button>
+        <Typography variant="h5" color="textSecondary" component="h2">
+          {i + 1}
+        </Typography>
       </CardActions>
     </Card>
   );
